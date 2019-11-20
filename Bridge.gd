61510f63ecd59f6eb319bd4d7ba72e0a265dcc44
ilariacuccaro <ilaria.cuccaro19@gmail.com>
2019-11-20 12:28:40 +0100

    Modification to the bridge

--- Bridge.gd
+++ Bridge.gd
@@ -1,12 +1,26 @@
 extends KinematicBody
 
-signal rot
-signal nrot
+var r = 0
+var move = false
 
 func _on_Player_bridge():
 	print("Player is on the bridge")
-	emit_signal("rot")
-
+	move = true
+	
+func _physics_process(delta):
+	if(move):
+		while(r < 2.2):
+			rotate_object_local(Vector3(1,0,0),deg2rad(0.2))
+			r = get_rotation_degrees().x
+			print(rotation, "  ->  ", r)
+		print("change")
+		while(r > -2.2):
+		#	set_rotation_degrees(Vector3(0,0,0))
+			rotate_object_local(Vector3(-1,0,0),deg2rad(0.2))
+			r = get_rotation_degrees().x
+			print(rotation, "  <-  ", r)
+		print("change")
+	
 func _exit_Player_bridge():
 	print("Player exit")
-	emit_signal("nrot")
+	move = false
--- Player.gd
+++ Player.gd
@@ -1,24 +1,29 @@
 extends RigidBody
 
+#var gravity = 9.8
+#var pos = Vector3(0,1,0)
+#var vel = Vector3()
 signal moveP
 signal moveB
 signal notmoveB
 
 func _ready():
-	connect("body_entered", self, "platform_enter")
-	connect("body_entered", self, "bridge_enter")
-	connect("body_exited", self, "bridge_exit")
+	connect("body_entered", self, "enter")
+	connect("body_exited", self, "exit")
 
+func _physics_process(delta):
+	get_gravity_scale()
+#		vel.y = gravity*delta
+#		move_and_slide(vel, pos)
 
-func platform_enter(body):
+func enter(body):
 	if body.get_name() == "Platformer":
 		print("collision with platform", body.get_name())
 		emit_signal("moveP")
-
-func bridge_enter(body):
 	if body.get_name() == "Bridge":
 		print("collision with a bridge", body.get_name())
 		emit_signal("moveB")
 
-func bridge_exit(body):
+func exit(body):
+	if body.get_name() == "Bridge":
 		emit_signal("notmoveB")
