69aa9cdc3ee63717a3827d66e8c51c31631d9aad
ilariacuccaro <ilaria.cuccaro19@gmail.com>
2019-11-21 12:04:15 +0100

    Second modification to the bridge

--- Bridge.gd
+++ Bridge.gd
@@ -3,24 +3,26 @@ extends KinematicBody
 var r = 0
 var move = false
 
-func _on_Player_bridge():
+func on_Player_bridge():
 	print("Player is on the bridge")
 	move = true
-	
+
+
 func _physics_process(delta):
 	if(move):
 		while(r < 2.2):
-			rotate_object_local(Vector3(1,0,0),deg2rad(0.2))
+#			rotate_object_local(Vector3(1,0,0),deg2rad(0.2))
+			rotate_object_local(Vector3(1,0,0),0.05)
 			r = get_rotation_degrees().x
-			print(rotation, "  ->  ", r)
-		print("change")
+			print("rotation bridge", rotation, "  ->  ", r)
+	#	print("change")
 		while(r > -2.2):
 		#	set_rotation_degrees(Vector3(0,0,0))
-			rotate_object_local(Vector3(-1,0,0),deg2rad(0.2))
+			rotate_object_local(Vector3(-1,0,0),0.05)
 			r = get_rotation_degrees().x
-			print(rotation, "  <-  ", r)
-		print("change")
+			print("rotation Bridge", rotation, "  <-  ", r)
+	#	print("change")
 	
-func _exit_Player_bridge():
-	print("Player exit")
+func exit_Player_bridge():
+	print("Player exit from bridge")
 	move = false
--- Player.gd
+++ Player.gd
@@ -6,24 +6,48 @@ extends RigidBody
 signal moveP
 signal moveB
 signal notmoveB
+var r = 0
+var move = false
+#var bridge = load("res://Bridge4.tscn") 
+#var node = bridge.instance()
+#var CollS
 
 func _ready():
 	connect("body_entered", self, "enter")
 	connect("body_exited", self, "exit")
+#	add_child(node)
+#	CollS = node.get_node("CollisionBridge")
+#	print("Nodo ottenuto: ", node, node.get_name())
+#	print(CollS)
 
-func _physics_process(delta):
-	get_gravity_scale()
-#		vel.y = gravity*delta
-#		move_and_slide(vel, pos)
 
 func enter(body):
 	if body.get_name() == "Platformer":
 		print("collision with platform", body.get_name())
 		emit_signal("moveP")
 	if body.get_name() == "Bridge":
-		print("collision with a bridge", body.get_name())
+		print("collision with a bridge / ", body.get_name())
+		move = true
 		emit_signal("moveB")
+	if body.get_name() == "Board":
+		print("enter on boards", body.get_name())
+	print("Collision whith: ", body.name)
+
+#func _physics_process(delta):
+#	if(move):
+#		while(r < 2.2):
+#				rotate_object_local(Vector3(1,0,0),0.05499499)
+#				r = get_rotation_degrees().x
+#				print("rotation Player", rotation, "  ->  ", r)
+		#	print("change")
+#		while(r > -2.2):
+			#	set_rotation_degrees(Vector3(0,0,0))
+#				rotate_object_local(Vector3(-1,0,0),0.05499499)
+#				r = get_rotation_degrees().x
+#				print("rotation Player", rotation, "  <-  ", r)
 
 func exit(body):
 	if body.get_name() == "Bridge":
+		print("exit from bridge")
+		move = false
 		emit_signal("notmoveB")
