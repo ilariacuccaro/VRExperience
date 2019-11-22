cf04329b04465fcf4fb7fca62b18edce0ceeb1e5
ilariacuccaro <ilaria.cuccaro19@gmail.com>
2019-11-22 12:40:02 +0100

    change environment

--- Platformer.gd
+++ Platformer.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 	if (move == true):
 		vel.y = force* delta
 		move_and_slide(vel, pos)
-#		print(self.transform.translated(Vector3()))
+		print(self.transform.translated(Vector3()))
 
 func _on_Platform_stopP():
 	move = false
