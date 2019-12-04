6bf1eb9dfef4430b2a2c0c0e0b81413d5efae03a
MaySantucci <may.santucci@gmail.com>
2019-12-04 15:43:23 +0100

    Fixed teleport for left hand.

--- Game.gd
+++ Game.gd
@@ -5,7 +5,7 @@ func _ready():
     if VR and VR.initialize():
         get_viewport().arvr = true
         get_viewport().hdr = false
-        get_viewport().rgba8_out = true
+        #get_viewport().rgba8_out = true
 
         OS.vsync_enabled = false
         Engine.target_fps = 90
